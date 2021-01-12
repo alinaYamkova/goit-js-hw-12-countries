3353fb0f59c595a476f987ee975cf5a2f3d0f512
alinaYamkova <alyamkova@yahoo.com>
2021-01-12 14:30:12 +0200

    виправляе помилки

--- src/index.js
+++ src/index.js
@@ -6,17 +6,23 @@ import returnResult from './js/markups';
 import errorMessage from './js/notifications';
 
 refs.searchFormRef.addEventListener('input', debounce(onSeachCountry, 500));
-refs.searchFormRef.addEventListener('blur', onClean);
+// refs.searchFormRef.addEventListener('blur', onClean);
 
 function onSeachCountry(event) {
   event.preventDefault();
   refs.listRef.innerHTML = '';
+  refs.searchFormRef.innerHTML = '';
+  refs.country_info.innerHTML = '';
   const inputValue = refs.searchFormRef.value;
+  if (!inputValue) {
+      return;
+  }
+  
   fetchCountries(inputValue).then(returnResult).catch(errorMessage);
 };
 
-function onClean(event) {
-  event.preventDefault();
-  const form = document.querySelector('.form-control');
-  form.value = '';
-}
+// function onClean(event) {
+//   event.preventDefault();
+//   const form = document.querySelector('.form-control');
+//   form.value = '';
+// }

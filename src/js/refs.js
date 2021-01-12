0b33553a3fb3904a877a3028355e31ffc514d0f9
alinaYamkova <alyamkova@yahoo.com>
2021-01-12 15:10:30 +0200

    Updates

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-  searchFormRef: document.querySelector('.search-form > input'),
-  listRef: document.querySelector('.js-countries_list'),
-  country_info: document.querySelector('.country_info')
-};
-
-export default refs;
